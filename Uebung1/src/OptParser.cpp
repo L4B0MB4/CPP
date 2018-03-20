cfe8cfd801b3f7d70387618a831d41b7950af92e
L4B0MB4 <metalfan65@gmail.com>
2018-03-20 18:11:15 +0100

    Improvement

--- Uebung1/src/OptParser.cpp
+++ Uebung1/src/OptParser.cpp
@@ -5,24 +5,55 @@
 bool CmdLineOptParser::Parse(int argc, char* argv[] )
 {
     argc--;
-    printf("Anzahl Args: %d \n",argc);
-
+    bool wasAntotherArg =false;
     for(int i = 1; i<=argc;i++)
     {
         if(argv[i] == nullptr)return false;
         if(strlen(argv[i]) <=1)return false;
-        char* info = argv[i] +2;
-        Option(argv[i][1],info);
+        if(argv[i][0]=='-')
+        {
+            if(strlen(argv[i])==2)
+            {   
+                if(i<argc)
+                {
+                    int j = i+1;
+                    bool isValue = false;
+                    if(argv[j][0]!='-')isValue=true;
+
+                    if(isValue)
+                    {
+                        char* info = argv[j];
+                        Option(argv[i][1],info);
+                        i++;
+                    }
+                    else{
+                        Option(argv[i][1],argv[i]+2);
+                    }
+                }
+                else Option(argv[i][1],argv[i]+2);
+            }
+            else if(strlen(argv[i])>2)
+            {
+                char* info = argv[i] +2;
+                if(info[0] == '=')info++;
+                Option(argv[i][1],info);
+            }
+        }
+        else return false;
+
+       
+        if(wasAntotherArg==true)i++;
+        wasAntotherArg=false;
     }
 
 
-    return false;
+    return true;
 }
 
 bool CmdLineOptParser::Option(const char c, const char* info)
 {
     printf("Char: %c\n",c);
     printf("Chars: %s\n",info);
-    printf(" bool %d",info[0] ==0);
+    printf(" bool %d \n",info[0] ==0);
     return true;
 }
