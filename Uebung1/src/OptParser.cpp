d990c6c2e2f46fd9264b2c6a8192b9f63ce60847
L4B0MB4 <metalfan65@gmail.com>
2018-03-26 15:43:21 +0200

    included comments

--- Uebung1/src/OptParser.cpp
+++ Uebung1/src/OptParser.cpp
@@ -3,35 +3,34 @@
 
 bool CmdLineOptParser::Parse(int argc, char* argv[] )
 {
-    argc--;
     bool wasAntotherArg =false;
-    for(int i = 1; i<=argc;i++)
+    for(int i = 1; i<argc;i++) // start with 2nd argument. 1. is fileinfo
     {
-        if(argv[i] == nullptr)return false;
+        if(argv[i] == nullptr)return false; 
         if(strlen(argv[i]) <=1)return false;
-        if(argv[i][0]=='-')
+        if(argv[i][0]=='-') // current param starts with "-" ? else -> error
         {
-            if(strlen(argv[i])==2)
+            if(strlen(argv[i])==2) // strlen ==2 -> bool or  value in 2 diffrent params
             {   
-                if(i<argc)
+                if(i<argc) // any other params left ? -> maybe not ool
                 {
                     int j = i+1;
                     bool isValue = false;
                     if(argv[j][0]!='-')isValue=true;
 
-                    if(isValue)
+                    if(isValue) // next param not starting with "-" -> value for current param
                     {
                         char* info = argv[j];
                         Option(argv[i][1],info);
                         i++;
                     }
                     else{
-                        Option(argv[i][1],argv[i]+2);
+                        Option(argv[i][1],argv[i]+2); //isbool
                     }
                 }
-                else Option(argv[i][1],argv[i]+2);
+                else Option(argv[i][1],argv[i]+2);//isbool
             }
-            else if(strlen(argv[i])>2)
+            else if(strlen(argv[i])>2) // everything else
             {
                 char* info = argv[i] +2;
                 if(info[0] == '=')info++;
@@ -39,8 +38,6 @@ bool CmdLineOptParser::Parse(int argc, char* argv[] )
             }
         }
         else return false;
-
-       
         if(wasAntotherArg==true)i++;
         wasAntotherArg=false;
     }
@@ -51,6 +48,7 @@ bool CmdLineOptParser::Parse(int argc, char* argv[] )
 
 bool CmdLineOptParser::Option(const char c, const char* info)
 {
+    
     if(info[0]==0 && c >0)return true;
     return false;
 }
