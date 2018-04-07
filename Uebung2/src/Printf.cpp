558415da15ab2408cbc7c532f7c8043265db93c9
L4B0MB4 <metalfan65@gmail.com>
2018-04-07 20:08:37 +0200

    Working. To Test !

--- Uebung2/src/Printf.cpp
+++ Uebung2/src/Printf.cpp
@@ -6,36 +6,40 @@
 
 char* Printf(char* dst, const void* end, const char* fmt,...)
 {
-    printf("%s\n",end);
-    const char* cEnd =reinterpret_cast<const char*>(fmt);
-    int length =std::char_traits<char>::length(cEnd);
-    void*  val;
+    const int* iEnd =reinterpret_cast<const int*>(end);
+    for(int i =0; i<(*iEnd);i++)
+    {
+        dst[i]=0;
+    }
+    const char* cFmt =reinterpret_cast<const char*>(fmt);
+    int length =std::char_traits<char>::length(cFmt);
+    char*  val;
     va_list vl;
     va_start(vl,fmt);
-    int bufferlength =100;
+    int bufferlength =*iEnd;
     char buffer[bufferlength];
     int offset=0;
     int coffset=0;
-    for(int i=0; i<length;++i){
-        dst[i+offset] = cEnd[i+coffset];
-        printf("dd %s \n",dst);
+    int i;
+    for( i=0; i<length && (i+offset)<(*iEnd);++i){
+        dst[i+offset] = cFmt[i+coffset];
+        if(cFmt[i+coffset]==0)
+        {
+            break;
+        }
         if(i+1+coffset<length)
         {
-            if(cEnd[i+coffset] == '%' && cEnd[i+1+coffset] != '%')
+            if(cFmt[i+coffset] == '%' && cFmt[i+1+coffset] != '%')
             {
-                val = va_arg(vl,void *);
-                getString(cEnd[i+1+coffset],val,buffer);
-                printf("DEST %s \n",dst);
-                printf("hier %s \n",buffer);
+                val = va_arg(vl,char *);
+                getString(cFmt[i+1+coffset],val,buffer);
                 for(int j=0;j<bufferlength;j++)
                 {
                     if(buffer[j]==0)
                     {
                         buffer[j]=0;
-                        offset=j-1;
+                        offset+=j-1;
                         coffset+=1;
-                        printf("CEND %c \n",cEnd[i+coffset]);
-                        printf("DEST %s \n",dst);
                         break;
                     }
                     else if(j<bufferlength)
@@ -45,14 +49,13 @@ char* Printf(char* dst, const void* end, const char* fmt,...)
                     }
                 }
             }
-            else if(cEnd[i+coffset] == '%' && cEnd[i+1+coffset] == '%'){
+            else if(cFmt[i+coffset] == '%' && cFmt[i+1+coffset] == '%'){
                 i+=1;
-                dst[i+offset]='%';
+                offset--;
             }
         }
     }
-
-    return NULL;
+    return &(dst[i+offset]);
 }
 
 void getString(char type, void* param, char* buffer)
@@ -61,7 +64,42 @@ void getString(char type, void* param, char* buffer)
     {
         case 'd': sprintf(buffer,"%d",param);break;;
         case 'u': sprintf(buffer,"%u",param);break;
-        case 'x': sprintf(buffer,"%c",param);break;
+        case 'x': sprintf(buffer,"0x%x",param);break;
         case 's': sprintf(buffer,"%s",param);break;
+        case 'b': 
+        printf("hier");
+       int  n =  *reinterpret_cast<int*>(&param); // wieso brauch ich erst die adresse von param?
+        printf("%d",n);
+        buffer[0]='0';
+        buffer[1]='b';
+        int i=0;
+        int umkehren=0;
+        if(n>=0)
+        {
+            while (n) {
+            if (n & 1)
+                buffer[i+2]='1';
+            else
+                buffer[i+2]='0';
+            ++i;
+            n >>= 1;
+            }
+        buffer[i+1]=0;
+        }
+        else if(n<0) // alles rückwärts
+        {
+            umkehren= 64;//64 bit zahlen
+            umkehren +=2;
+            while (n&& i <= umkehren-2) {
+                if (n & 1)
+                    buffer[umkehren-i]='1';
+                else
+                    buffer[umkehren-i]='0';
+                ++i;
+                n >>= 1;
+            }
+            buffer[umkehren+1]=0;
+        }
+        break;
     }
 }
--- Uebung2/src/main.cpp
+++ Uebung2/src/main.cpp
@@ -5,12 +5,13 @@ int main(int argp, char** args)
 {
     printf("%d \n",argp);
     printf("%s\n",args[0]);
-    int maxlen =100;
+    int maxlen =1000;
     char buffer[maxlen];
     char name[] = "Lars";
-    int age=21, b =55;
+    int age=21, b =55,c=-700;
 
-    Printf(buffer,&maxlen,"Hallo ich bin %s und bin %d! tada",name, age, b);
-    printf("BUFFER: %s",buffer);
+    char* x = Printf(buffer,&maxlen,"Hallo ich bin %s %% %% und bin %d! tada %x %b",name, age, b, c);
+    printf("%s \n",buffer);
+    printf("%c \n",*x);
     return 0;
 }
