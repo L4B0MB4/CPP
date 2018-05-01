d8556029ecbb84f0d91552cd3f9f5c67a400a72a
L4B0MB4 <metalfan65@gmail.com>
2018-05-01 16:24:10 +0200

    works <3

--- Uebung5/include/PoolAllocator.h
+++ Uebung5/include/PoolAllocator.h
@@ -6,23 +6,60 @@ class PoolAllocator: public IHeap
 {
     public:
 
+        PoolAllocator():
+            buffer{},
+            free{}
+        {
+            for(size_t i=0;i<blockCount;i++)
+            {
+                free[i]=1;
+            }
+        }
+
         void* Allocate(size_t sizeInBytes)
         {
             printf("%d \n",sizeInBytes);
-            return nullptr;
+            if(sizeInBytes>Available())
+            {
+                return nullptr;
+            }
+            else
+            {
+                size_t i=0;
+                for(;i<blockCount;i++)
+                {
+                    if(free[i]==1)
+                    {
+                        free[i]=0;
+                        break;
+                    }
+                }
+                return buffer+i*blockSize;
+            }
         }
 
         void Deallocate(void* p)
         {
-            printf("p %d \n",p);
+            char* pp = reinterpret_cast<char*>(p);
+            printf("pointer %d\n",pp-buffer);
+            free[(pp-buffer)/blockSize]=1;
         }
         size_t Available() const
         {
-            return 1;
+            size_t av=0;
+            for(size_t i=0;i<blockCount;i++)
+            {
+                if(free[i]==1)
+                {
+                    ++av;
+                }
+            }
+            return av*blockSize;
         }
     private:
         char buffer[blockCount*blockSize];
-        size_t size = blockCount*blockSize;
+        size_t maxSize = blockCount*blockSize;
+        char free[blockCount];
 };
 
 #define CREATE(varName, blockCount, blockSize) PoolAllocator<blockCount,blockSize> varName; 
--- Uebung5/src/main.cpp
+++ Uebung5/src/main.cpp
@@ -1,7 +1,8 @@
 #include "PoolAllocator.h"
 #include <stdio.h>
+#include <new>
 
-class Test{
+class Foo{
     public:
         int test=12;
         int test2 =13;
@@ -11,8 +12,17 @@ int main(int argc, char** args)
 {
     printf("%d %s\n",argc,args[argc]);
 
-    CREATE(p,10,20);
-    Test t = *reinterpret_cast<Test*>(p.Allocate(sizeof(Test)));
-    printf("%d \n",t.test);
+    CREATE(p,2,8);
+    Foo* foo = new(p.Allocate(sizeof(Foo))) Foo;
+    Foo* ff = new(p.Allocate(sizeof(Foo))) Foo;
+    ff->test = ff->test +20;
+    printf("%d \n",ff->test);
+    printf("%d \n",foo->test);
+    printf("%d \n",p.Available());
+    p.Deallocate(ff);
+    Foo* fff = new(p.Allocate(sizeof(Foo))) Foo;
+    fff->test = fff->test +60;
+    printf("%d \n",fff->test);
+    printf("%d \n",ff->test);
 
 }
